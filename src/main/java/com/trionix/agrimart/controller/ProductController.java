f242f98450653f6a6c74aedc765c8dea45754a2c
DeshinaWit <deshinawit@gmail.com>
2022-06-19 10:38:43 +0530

    Implemented product controller

--- src/main/java/com/trionix/agrimart/controller/ProductController.java
+++ src/main/java/com/trionix/agrimart/controller/ProductController.java
@@ -3,54 +3,45 @@ package com.trionix.agrimart.controller;
 import com.trionix.agrimart.dto.NewProductDto;
 import com.trionix.agrimart.entity.Product;
 import com.trionix.agrimart.repository.ProductRepository;
+import com.trionix.agrimart.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class ProductController {
 
-    private final ProductRepository productRepository;
+    private final ProductService productService;
 
-    public ProductController(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
-    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/products")
+    public ResponseEntity<Product> addNewProduct(@RequestBody NewProductDto productDto) {
 
-    public ResponseEntity<Product> addNewProduct(@RequestBody Product NewOrderDto) {
-        Product product = new Product();
-        product.setProductId(NewOrderDto.getProductId());
-        product.setProductTitle(NewOrderDto.getProductTitle());
-        product.setCategory(NewOrderDto.getCategory());
-        product.setDescription(NewOrderDto.getDescription());
-        product.setQuantity(NewOrderDto.getQuantity());
-        product.setPrice(NewOrderDto.getPrice());
-
-        return ResponseEntity.status(201).body(this.productRepository.save(product));
+        var product = productService.addProduct(productDto);
+        return ResponseEntity.ok(product);
     }
-      @CrossOrigin(origins = "http://localhost:3000")
 
-        @GetMapping("/getproducts")
+        @GetMapping("/products")
         public List<Product> getAllProducts () {
-        return productRepository.findAll();
+        return productService.findAll();
+
         }
+
 
         @GetMapping("/products/{id}")
-        public String getOneProduct (@PathVariable String id){
-            System.out.println(id);
-            return "getOneProduct";
+        public Product getOneProduct (@PathVariable String id){
+            return productService.findOne(id);
         }
+
         @DeleteMapping("/products/{id}")
-        public String deleteProduct (@PathVariable String id){
-            System.out.println(id);
-            return "deleteProduct";
+        public void deleteProduct (@PathVariable String id){
+            productService.deleteOne(id);
         }
         @PutMapping("/products/{id}")
-        public String updateProduct (@RequestBody NewProductDto body, @PathVariable String id){
-            System.out.println(body);
-            return "updateProduct";
+        public Product updateProduct (@RequestBody NewProductDto body, @PathVariable String id){
+            return productService.updateOne(id, body);
         }
 
     }
--- src/main/java/com/trionix/agrimart/dto/NewProductDto.java
+++ src/main/java/com/trionix/agrimart/dto/NewProductDto.java
@@ -1,74 +1,17 @@
 package com.trionix.agrimart.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NewProductDto {
-    private Long productId;
     private String productTitle;
     private String category;
     private String description;
-    private int quantity;
-    private double price;
-
-    public NewProductDto() {
-    }
-
-    public NewProductDto(Long productId, String productTitle, String category, String description, int quantity, double price) {
-        this.productId = productId;
-        this.productTitle = productTitle;
-        this.category = category;
-        this.description = description;
-        this.quantity = quantity;
-        this.price = price;
-    }
-
-    public Long getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Long productId) {
-        this.productId = productId;
-    }
-
-    public String getProductTitle() {
-        return productTitle;
-    }
-
-    public void setProductTitle(String productTitle) {
-        this.productTitle = productTitle;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
+    private Double quantity;
+    private Double price;
 }
 
