a43de2177cdd8e6b2086956cc20fa1f8a8d065d1
Imesha Basnayake <81801951+Imesha599@users.noreply.github.com>
2022-06-20 13:30:55 +0530

    Update CustomerController.java

--- src/main/java/com/trionix/agrimart/controller/CustomerController.java
+++ src/main/java/com/trionix/agrimart/controller/CustomerController.java
@@ -1,21 +1,40 @@
 package com.trionix.agrimart.controller;
 
-
-import com.trionix.agrimart.dto.NewCustomerDto;
+import com.example.trionix.agrimart.dto.NewCustomerDto;
+import com.example.trionix.agrimart.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.example.trionix.agrimart.repository.CustomerRespository;
+
+@Document("Customers")
 
 @RestController
 public class CustomerController {
+   @Autowired
+    private final CustomerRespository customerRepository;
+
+    public CustomerController(CustomerRespository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     @PostMapping("/customers")
-    public String createCustomer(@RequestBody NewCustomerDto body){
-        System.out.println(body);
-        return "Post All Customers";
+    public ResponseEntity<Customer> createCustomers(@RequestBody Customer NewCustomerDto) {
+        Customer customer =new Customer();
+        customer.setFirstName( NewCustomerDto.getFirstName());
+        customer.setLastName( NewCustomerDto.getLastName());
+        customer.setAge(NewCustomerDto.getAge());
+        customer.setPhoneNumber( NewCustomerDto.getPhoneNumber());
+        customer.setAddress( NewCustomerDto.getAddress());
+
+        return ResponseEntity.status(201).body(this.customerRepository.save(customer));
+
     }
     @GetMapping("/customers")
     public String getAllCustomer(){
         return "getAllCustomer";
     }
-
     @GetMapping("/customers/{id}")
     public String getOneCustomer(@PathVariable String id){
         System.out.println(id);
@@ -23,6 +42,7 @@ public class CustomerController {
     }
     @DeleteMapping("/customers/{id}")
     public String deleteCustomer(@PathVariable String id){
+        System.out.println(id);
         return "deleteCustomer";
     }
     @PutMapping("/customers/{id}")
@@ -30,6 +50,7 @@ public class CustomerController {
         System.out.println(body);
         return "updateCustomer";
     }
+
 }
 
 
